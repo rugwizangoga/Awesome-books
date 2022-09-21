55696b6169dbcd14f8ae6a7df961c174790a5d75
Joshua <ezejoshuac@gmail.com>
2022-09-21 10:42:31 +0100

    Create add and remove books functionality

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const books = document.getElementById('books');
-const add = document.getElementById('btn');
+const books = document.getElementById("books");
+const add = document.getElementById("btn");
 let storedbooks = [];
 let count = 0;
 function printbook() {
-    const bookdata = JSON.parse(window.localStorage.getItem('books'));
-    storedbooks = bookdata;
-    books.replaceChildren();
-       storedbooks.forEach(book => {
-       const newdiv = document.createElement('div');
-       newdiv.innerHTML = `
+  const bookdata = JSON.parse(window.localStorage.getItem("books"));
+  storedbooks = bookdata;
+  books.replaceChildren();
+  storedbooks.forEach((book) => {
+    const newdiv = document.createElement("div");
+    newdiv.innerHTML = `
        <div class="book">
        <p id="title">
         ${book.title}
@@ -19,20 +19,39 @@ function printbook() {
        <button type="button" class="remove" id="${book.id}">Remove</button>
        </div>
        `;
-   books.append(newdiv);
-   count= parseInt(book.id);
-});
-
+    books.append(newdiv);
+    count = parseInt(book.id);
+  });
+  const remove = document.querySelectorAll(".remove");
+  remove.forEach((rem) => {
+    rem.addEventListener("click", (rem) => {
+      const { id } = rem.target;
+      ids = id;
+      const btns = document.getElementById(ids);
+      btns.parentElement.remove();
+      storedbooks = storedbooks.filter((book) => book.id !== ids);
+      window.localStorage.setItem("books", JSON.stringify(storedbooks));
+    });
+  });
 }
 
-if(localStorage.getItem('books')){
-printbook();
+if (localStorage.getItem("books")) {
+  printbook();
 }
 
 const fdata = {
-    id: count.toString(),
-    title: '',
-    author: ''
-  };
-const stringfiedbooks= [];
+  id: count.toString(),
+  title: "",
+  author: "",
+};
+// const stringfiedbooks = [];
 
+add.addEventListener("click", () => {
+  count += 1;
+  fdata.id = count.toString();
+  fdata.title = document.forms[0].elements[0].value;
+  fdata.author = document.forms[0].elements[1].value;
+  storedbooks.push(fdata);
+  window.localStorage.setItem("books", JSON.stringify(storedbooks));
+  printbook();
+});

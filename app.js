6716909ed49ee3c526a10a4bc7463ccd804db757
rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-09-22 12:14:59 +0100

    refactored it with classes

--- app.js
+++ app.js
@@ -1,62 +1,71 @@
-const books = document.getElementById('books');
-const add = document.getElementById('btn');
-let storedbooks = [];
-let count = 0;
-
-const fdata = {
-  id: count.toString(),
-  title: '',
-  author: '',
+class Fdata  {
+  constructor(id, title, author){
+  this.id = id.toString(),
+  this.title = title,
+  this.author = author
+}
 };
 
-function printbook() {
-  const bookdata = JSON.parse(window.localStorage.getItem('books'));
-  storedbooks = bookdata;
-  books.replaceChildren();
-  storedbooks.forEach((book) => {
-    const newdiv = document.createElement('div');
-    newdiv.innerHTML = `
-       <div class="book">
-       <p id="title">
-        ${book.title}
-        </p>
-       <p id="author">
-       ${book.author}
-        </p>
-       <button type="button" class="remove" id="${book.id}">Remove</button>
-       </div>
-       `;
-    books.append(newdiv);
-    count = parseInt(book.id, 10);
-  });
-  const remove = document.querySelectorAll('.remove');
-  remove.forEach((rem) => {
-    rem.addEventListener('click', (rem) => {
-      const { id } = rem.target;
-      const btns = document.getElementById(id);
-      btns.parentElement.remove();
-      storedbooks = storedbooks.filter((book) => book.id !== id);
-      window.localStorage.setItem('books', JSON.stringify(storedbooks));
+class Books {
+  storedbooks = [];
+  count = 0;
+  constructor(book){
+    this.book= book;
+  }
+
+  printbook() {
+    const books = document.getElementById('books');
+    const bookdata = JSON.parse(window.localStorage.getItem('books'));
+    this.storedbooks = bookdata;
+    books.replaceChildren();
+    this.storedbooks.forEach((book) => {
+      const newdiv = document.createElement('div');
+      newdiv.innerHTML = `
+         <div class="book">
+         <p id="title">
+          ${book.title}
+          </p>
+         <p id="author">
+         ${book.author}
+          </p>
+         <button type="button" class="remove" id="${book.id}">Remove</button>
+         </div>
+         `;
+      books.append(newdiv);
+      this.count = parseInt(book.id, 10);
     });
-  });
+    const remove = document.querySelectorAll('.remove');
+    remove.forEach((rem) => {
+      rem.addEventListener('click', (rem) => {
+        const { id } = rem.target;
+        const btns = document.getElementById(id);
+        btns.parentElement.remove();
+        this.storedbooks = this.storedbooks.filter((b) => b.id !== id);
+        window.localStorage.setItem('books', JSON.stringify(this.storedbooks));
+      });
+    });
+  }
+
+  Addbook(title, author) {
+    this.count += 1;
+    this.book = new Fdata(this.count,title,author);
+    this.storedbooks.push(this.book);
+    window.localStorage.setItem('books', JSON.stringify(this.storedbooks));
+  }
+  
 }
 
-function Addbook(title, author) {
-  count += 1;
-  fdata.id = count.toString();
-  fdata.title = title;
-  fdata.author = author;
-  storedbooks.push(fdata);
-  window.localStorage.setItem('books', JSON.stringify(storedbooks));
-}
+const mystore =new Books();
 
 if (localStorage.getItem('books')) {
-  printbook();
+  mystore.printbook();
 }
+
+const add = document.getElementById('btn');
 
 add.addEventListener('click', () => {
   const booktitle = document.forms[0].elements[0].value;
   const bookauthor = document.forms[0].elements[1].value;
-  Addbook(booktitle, bookauthor);
-  printbook();
+  mystore.Addbook(booktitle, bookauthor);
+  mystore.printbook();
 });

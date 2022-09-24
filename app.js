0c46eacf2b46263dabc06be3a31d8c03e93b9853
rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-09-24 16:03:14 +0100

    removed the linter errors

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 /* eslint-disable max-classes-per-file */
 
-const addnew = document.getElementById("addnew");
-const read = document.getElementById("read");
-const contact = document.getElementById("contact");
-const now = document.getElementById("now");
-const go = document.getElementById("go");
-const then = document.getElementById("then");
-const navitem = document.querySelectorAll(".navitem");
+const addnew = document.getElementById('addnew');
+const read = document.getElementById('read');
+const contact = document.getElementById('contact');
+const now = document.getElementById('now');
+const go = document.getElementById('go');
+const then = document.getElementById('then');
+const navitem = document.querySelectorAll('.navitem');
 
-addnew.classList.add("active");
-contact.classList.add("active");
-now.classList.add("active");
+addnew.classList.add('active');
+contact.classList.add('active');
+now.classList.add('active');
 
 navitem.forEach((item) => {
-  item.addEventListener("click", (nav) => {
+  item.addEventListener('click', (nav) => {
     const { id } = nav.target;
-    if (id === "go") {
-      addnew.classList.remove("active");
-      read.classList.add("active");
-      contact.classList.add("active");
-      go.classList.add("active");
-      now.classList.remove("active");
-      then.classList.remove("active");
-    } else if (id === "then") {
-      contact.classList.remove("active");
-      addnew.classList.add("active");
-      read.classList.add("active");
-      go.classList.remove("active");
-      now.classList.remove("active");
-      then.classList.add("active");
+    if (id === 'go') {
+      addnew.classList.remove('active');
+      read.classList.add('active');
+      contact.classList.add('active');
+      go.classList.add('active');
+      now.classList.remove('active');
+      then.classList.remove('active');
+    } else if (id === 'then') {
+      contact.classList.remove('active');
+      addnew.classList.add('active');
+      read.classList.add('active');
+      go.classList.remove('active');
+      now.classList.remove('active');
+      then.classList.add('active');
     } else {
-      read.classList.remove("active");
-      addnew.classList.add("active");
-      contact.classList.add("active");
-      go.classList.remove("active");
-      now.classList.add("active");
-      then.classList.remove("active");
+      read.classList.remove('active');
+      addnew.classList.add('active');
+      contact.classList.add('active');
+      go.classList.remove('active');
+      now.classList.add('active');
+      then.classList.remove('active');
     }
   });
 });
@@ -56,13 +56,13 @@ class Books {
   }
 
   printbook() {
-    const books = document.getElementById("books");
-    const bookdata = JSON.parse(window.localStorage.getItem("books"));
+    const books = document.getElementById('books');
+    const bookdata = JSON.parse(window.localStorage.getItem('books'));
     this.storedbooks = bookdata;
     books.replaceChildren();
     let num = 0;
     this.storedbooks.forEach((book) => {
-      const newdiv = document.createElement("div");
+      const newdiv = document.createElement('div');
       num += 1;
       newdiv.innerHTML = `
       <p>
@@ -70,21 +70,21 @@ class Books {
       </p>
       <button type="button" class="remove" id="${book.id}">Remove</button>
          `;
-      newdiv.className = "book";
+      newdiv.className = 'book';
       if (num % 2 === 0) {
-        newdiv.classList.add("two");
+        newdiv.classList.add('two');
       }
       books.append(newdiv);
       this.count = parseInt(book.id, 10);
     });
-    const remove = document.querySelectorAll(".remove");
+    const remove = document.querySelectorAll('.remove');
     remove.forEach((rem) => {
-      rem.addEventListener("click", (rem) => {
+      rem.addEventListener('click', (rem) => {
         const { id } = rem.target;
         const btns = document.getElementById(id);
         btns.parentElement.remove();
         this.storedbooks = this.storedbooks.filter((b) => b.id !== id);
-        window.localStorage.setItem("books", JSON.stringify(this.storedbooks));
+        window.localStorage.setItem('books', JSON.stringify(this.storedbooks));
       });
     });
   }
@@ -94,19 +94,19 @@ class Books {
     this.book = new Fdata(this.count, title, author);
     this.storedbooks.push(this.book);
     document.forms[0].reset();
-    window.localStorage.setItem("books", JSON.stringify(this.storedbooks));
+    window.localStorage.setItem('books', JSON.stringify(this.storedbooks));
   }
 }
 
 const mystore = new Books();
 
-if (localStorage.getItem("books")) {
+if (localStorage.getItem('books')) {
   mystore.printbook();
 }
 
-const add = document.getElementById("btn");
+const add = document.getElementById('btn');
 
-add.addEventListener("click", () => {
+add.addEventListener('click', () => {
   const booktitle = document.forms[0].elements[0].value;
   const bookauthor = document.forms[0].elements[1].value;
   mystore.Addbook(booktitle, bookauthor);
